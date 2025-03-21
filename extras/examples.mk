b8cceb366ae64e1edcd80fd897d8b34412d89316
Daniel C <brikbusters@gmail.com>
2025-03-20 21:02:40 -0400

    Add CMake build system

--- extras/examples.mk
+++ extras/examples.mk
@@ -4,8 +4,8 @@
 
 TESTER_FILES := $(addprefix test/,drawtests.o images.o main.o menus.o page1.o page2.o page3.o page4.o page5.o page6.o page7.o page7a.o page7b.o page7c.o page11.o page12.o page13.o page14.o page15.o page16.o page17.o spaced.o)
 
-TESTER_FILES_WIN := $(call convert_target_to,$(TESTER_FILES),w)
-tester.exe: $(TESTER_FILES_WIN) test/resources.res libui_win64.a
+TESTER_FILES_WIN := $(call convert_target,$(TESTER_FILES),w)
+tester.exe: $(TESTER_FILES_WIN) test/resources.res libui_x86_64_win.a
 	$(CC) -static $(TESTER_FILES_WIN) libui_x86_64_win.a test/resources.res $(LIBS) -o tester.exe
 
 TESTER_FILES_UNIX := $(call convert_target_to,$(TESTER_FILES),l)
--- include/ui.h
+++ include/ui.h
@@ -4104,7 +4104,7 @@ _UI_EXTERN void uiWindowSetIcon(uiWindow *w, const void *data, size_t length);
 
 // WIP - not supported on all platforms yet
 typedef struct uiScroll uiScroll;
-_UI_EXTERN uiScroll *uiNewScroll();
+_UI_EXTERN uiScroll *uiNewScroll(void);
 _UI_EXTERN void uiScrollSetChild(uiScroll *scroll, uiControl *ctl);
 
 // Experimental - use uiAttribute API on normal widgets
